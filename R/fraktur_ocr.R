7963958ba3e92a726a789cd8de5b73627e897a82
apsteinmetz <apsteinmetz@yahoo.com>
2020-06-13 15:38:33 -0400

    basic workflow complete

--- R/fraktur_ocr.R
+++ R/fraktur_ocr.R
@@ -5,10 +5,16 @@ library(magick)
 library(tokenizers)
 library(tidytext)
 library(hunspell)
-library(magrittr)
+library(translateR)
 
-path = "C:/Users/Arthur/Documents/Scans/"
+# Fraktur training set must be loaded. Only run once.
+# tesseract::tesseract_download("frk")
+google.api.key = Sys.getenv("GGMAP_GOOGLE_API_KEY")
+#path = "C:/Users/Arthur/Documents/Scans/"
+path = "C:/Users/Arthur/Pictures/ControlCenter4/Scan/"
+file_name = "light_300.jpg"
 
+#scan at 300dpi, highest brighness setting
 ocr_lines <- function(file,engine = "frk"){
    tesseract::ocr(file,engine = engine) %>%
       tokenize_regex(pattern = "\\n") %>%
@@ -25,46 +31,119 @@ ocr_vector <- function(file,engine = "frk"){
       str_remove_all("\\n")
 }
 
-lines_raw <- ocr_lines(paste0(path,"frk_clean.jpg"))
-words_raw <- ocr_words(paste0(path,"frk_clean.jpg"))
+# PROCESS PAGE IMAGE ----------------------------------------------------
+process_page <- function(file_name){
+   # IMAGE PROCESSING FOR BETTER OCR, IF NECESSARY -----------------------
+   #page_img <- image_read(paste0(path,file_name))
+   #page_img <- image_enhance(page_img)
+   #page_img <- image_contrast(page_img,sharpen = 10)
+   #page_img <- image_quantize(page_img,max = 2,colorspace = "gray")
+   #page_img  # Preview image assuming you use RStudio
+   #image_write(page_img,paste0(path,file_name))
 
-# convert "ſ" to s
-words <- words_raw  %>% map(str_replace_all,"ſ","s")
-lines <- lines_raw %>%
-   mutate(line = str_replace_all(line,"ſ","s")) %>%
-   mutate(line = str_remove_all(line,"[|*]"))
 
-list_dictionaries()
-lang =  "de_DE"
-dictionary(lang = lang)
+   # USE TESSERACT OCR ENGINE ----------------------------------------
+   lines_raw <- ocr_lines(paste0(path,file_name))
 
-words_df <- enframe(words[[1]],name=NULL,value="word") %>%
-   mutate(correct=hunspell_check(word,dict = lang)) %>%
-   mutate(suggestions=hunspell_suggest(word,dict = lang))
+   # convert "ſ" to s
+   lines <- lines_raw %>%
+      mutate(line = str_replace_all(line,"ſ","s")) %>%
+      mutate(line = str_remove_all(line,"[\\|\\*<>»]")) %>%
+      mutate(line = ifelse(str_detect(line,"[a-zA-Z]3"),str_replace(line,"3","s"),line)) %>%
+      mutate(line = str_squish(line)) %>%
+      mutate(line = str_replace_all(line,"Ginleitung","Einleitung")) %>%
+      mutate(line = str_replace_all(line,"=","-"))
 
-# get most likely term
-best_suggestion <- words_df$suggestions %>%
-   map(pluck,1,.default=NA) %>%
-   unlist() %>%
-   enframe(name=NULL,value="suggestion")
+   words <- tokenizers::tokenize_words(lines$line,strip_punct = FALSE) %>%
+      unlist(recursive = FALSE)
 
-# substitute first alternate
-words_df2 <- words_df %>%
-   bind_cols(best_suggestion) %>%
-   select(-suggestions) %>%
-   # take first suggestion for "incorrect" words or....
-   #  mutate(word_alt = ifelse(correct,word,suggestion)) %>%
-   # Only correct capitalization errors beacuse we think Google translate
-   # has a bigger word list and is smarter about misspellings
-   mutate(word_alt = ifelse(str_to_title(word)==suggestion,suggestion,word)) %>%
-   mutate(word_alt = ifelse(str_detect(word,".-,"),word,word_alt)) %>%
-   mutate(word_alt = ifelse(is.na(word_alt)," ",word_alt)) %>%
-   {.}
 
-pull(words_df2,word_alt) %>%
-   paste(collapse = " ") %>%
-   str_squish() %>%
-   str_replace_all("[ ]+\\.","\\.") %>%
-   str_replace_all("[ ]+,",",") %>%
-   writeClipboard(format = 13) # format 13 is unicode to allow transfer of special fraktur characters to clip
-# send to google translate
+   # USE HUNSPELL SPELL CHECKER IN GERMAN
+   # USE RSTUDIO Tools/Global/Spelling TO SELECT GERMAN LANGUAGE TO ENSURE IT IS LOADED
+   #list_dictionaries()
+   lang =  "de_DE"
+   #dictionary(lang = lang)
+
+   words_df <- enframe(words,name=NULL,value="word") %>%
+      mutate(correct=hunspell_check(word,dict = lang)) %>%
+      mutate(suggestions=hunspell_suggest(word,dict = lang))
+
+   # get most likely term
+   best_suggestion <- words_df$suggestions %>%
+      map(pluck,1,.default=NA) %>%
+      unlist() %>%
+      enframe(name=NULL,value="suggestion")
+
+   # choose alternate word
+   words_df2 <- words_df %>%
+      bind_cols(best_suggestion) %>%
+      select(-suggestions) %>%
+      # take first suggestion for "incorrect" words or....
+      #  mutate(word_alt = ifelse(correct,word,suggestion)) %>%
+      # Only correct capitalization errors beacuse we think Google translate
+      # has a bigger word list and is smarter about misspellings
+      mutate(suggestion = ifelse(is.na(suggestion)," ",suggestion)) %>%
+      mutate(word_alt = word) %>%
+      mutate(word_alt = ifelse(str_to_title(word)==suggestion,suggestion,word_alt)) %>%
+      {.}
+
+   cleaned <- pull(words_df2,word_alt) %>%
+      paste(collapse = " ") %>%
+      str_squish() %>%
+      str_replace_all("- -","--")
+
+   cleaned %>%
+      writeClipboard(format = 13) # format 13 is unicode to allow transfer of special fraktur characters to clip
+
+
+   # send to google translate
+   # getGoogleLanguages()
+   # note we use a character vector not a data frame which is slower and provides less context (less accurate).
+   translated <- translate(content.vec = cleaned,
+                           google.api.key = google.api.key,
+                           source.lang = "de",
+                           target.lang = "en")
+
+   # paginate translation
+   words_trans <- tokenizers::tokenize_words(translated,strip_punct = FALSE) %>%
+      unlist(recursive = FALSE)
+
+   words_count <- tokenizers::tokenize_words(translated,strip_punct = FALSE) %>%
+      unlist(recursive = FALSE) %>%
+      length()
+   words_per_line = round(words_count / nrow(lines))
+   #build page data frame  do it old school loop since we have to test for punctuation
+   word_pos = 1
+   cur_page <- list()
+   for (i in 1:nrow(lines)){
+      cur_line = ""
+      line_pos = 1
+      while(line_pos < words_per_line + 1){
+         cur_word <- ifelse(word_pos > words_count,"",words_trans[word_pos])
+         cur_line = paste(cur_line,cur_word)
+         word_pos <- word_pos + 1
+#         if(str_detect(words_trans[word_pos],"[\\w]")){
+#            line_pos <- line_pos + 1
+#         }
+         line_pos <- line_pos + 1
+      }
+      cur_page <- c(cur_page,cur_line)
+   }
+
+   en_page <- unlist(cur_page,recursive = FALSE) %>% enframe(name=NULL,value="English") %>%
+      mutate(English = str_replace_all(English," \\,",",")) %>%
+      mutate(English = str_replace_all(English," \\.",".")) %>%
+      mutate(English = str_squish(English))
+
+
+   lines_both <- lines %>% rename(German = line) %>%
+      bind_cols(en_page)
+
+   return(tibble(img_file=file_name,
+                 ocr_engine = "Tesseract",
+                 translator = "Google",
+                 text = list(lines_both)))
+}
+
+# main loop -----------------------------------------------------------------------------
+processed <- process_page(file_name)
