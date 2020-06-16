d9a9115e05363d817036407d04419b3d14a76423
apsteinmetz <apsteinmetz@yahoo.com>
2020-06-15 22:11:56 -0400

    scan and translate done

--- R/fraktur_ocr.R
+++ R/fraktur_ocr.R
@@ -8,53 +8,91 @@ library(hunspell)
 library(translateR)
 
 # Fraktur training set must be loaded. Only run once.
-# tesseract::tesseract_download("frk")
+# this training data in the official repo is old an not great
+#tesseract::tesseract_download("frk")
+
+#manually install this instead
+dl <- "https://github.com/tesseract-ocr/tessdata_best/raw/master/frk.traineddata"
+# this is pretty good
+dl <- #https://github.com/tesseract-ocr/tessdata/blob/master/deu_frak.traineddata
+
+# this crashes RSTudio
+#download.file( "https://ub-backup.bib.uni-mannheim.de/~stweil/ocrd-train/data/Fraktur_5000000/tessdata_best/Fraktur_50000000.334_450937.traineddata",
+#               dest = "data/Fraktur_50000000.334_450937.traineddata")
+
+
 google.api.key = Sys.getenv("GGMAP_GOOGLE_API_KEY")
-path = "C:/Users/Arthur/Documents/Scans/"
+path = "img/"
+tesseract_info()
+ocr_engine = "deu_frak"
+#ocr_engine = "frk"
 #path = "C:/Users/Arthur/Pictures/ControlCenter4/Scan/"
-file_name = "Neu_Schowe_Church_19.jpg"
+file_name = "Neu_Schowe_Church_65.jpg"
 
 #scan at 300dpi, highest brighness setting
-ocr_lines <- function(file,engine = "frk"){
+ocr_lines <- function(file,engine = ocr_engine){
    tesseract::ocr(file,engine = engine) %>%
       tokenize_regex(pattern = "\\n") %>%
       unlist() %>%
       enframe(name = "line_num",value="line")
 }
 
-ocr_words <- function(file,engine = "frk"){
+ocr_words <- function(file,engine = ocr_engine){
    tesseract::ocr(file,engine = engine) %>%
       tokenize_words(strip_punct = FALSE)
 }
-ocr_vector <- function(file,engine = "frk"){
+ocr_vector <- function(file,engine = ocr_engine){
    tesseract::ocr(file,engine = engine) %>%
       str_remove_all("\\n")
 }
 
+
 # PROCESS PAGE IMAGE ----------------------------------------------------
 process_page <- function(file_name){
    # IMAGE PROCESSING FOR BETTER OCR, IF NECESSARY -----------------------
-   #page_img <- image_read(paste0(path,file_name))
-   #page_img <- image_enhance(page_img)
-   #page_img <- image_contrast(page_img,sharpen = 10)
-   #page_img <- image_quantize(page_img,max = 2,colorspace = "gray")
-   #page_img  # Preview image assuming you use RStudio
-   #image_write(page_img,paste0(path,file_name))
+   # Trial and error
+   print(paste0("Processing image ",file_name))
+   page_img <- image_read(paste0(path,file_name)) ; page_img
+   # page_img <- image_contrast(page_img,sharpen = 100) ; page_img
+   # page_img <- image_trim(page_img,fuzz = 50) ; page_img
+   # page_img <- image_modulate(page_img,brightness = 90,saturation = 50); page_img
+   page_img <- image_enhance(page_img); page_img
+   #page_img <- image_quantize(page_img,max = 2,colorspace = "gray") ; page_img
+   #page_img <- image_lat(page_img, geometry = '2x2-10%') ; page_img
+
+   lines_test <- ocr_lines(page_img,engine = "deu_frak") %>%
+      full_join(ocr_lines(page_img,engine = "frk"), by = "line_num") %>%
+      rename(deu_frak = line.x,frk = line.y) %>%
+      mutate(frk = str_replace_all(frk,"ſ","s"))
+   View(lines_test)
 
 
    # USE TESSERACT OCR ENGINE ----------------------------------------
-   lines_raw <- ocr_lines(paste0(path,file_name))
+   # lines_raw <- ocr_lines(paste0(path,file_name))
+   print("Extracting text ")
+   lines_raw <- ocr_lines(page_img)
 
-   # convert "ſ" to s
-   German <- lines_raw %>%
-      mutate(line = str_replace_all(line,"ſ","s")) %>%
-      mutate(line = str_remove_all(line,"[\\|\\*<>»]")) %>%
-      mutate(line = ifelse(str_detect(line,"[a-zA-Z]3"),str_replace(line,"3","s"),line)) %>%
-      mutate(line = str_squish(line)) %>%
-      mutate(line = str_replace_all(line,"Ginleitung","Einleitung")) %>%
-      mutate(line = str_replace_all(line,"=","-"))
+   print("Refining text ")
 
-   words <- tokenizers::tokenize_words(lines$German,strip_punct = FALSE) %>%
+   # discard first line if it's the page number
+   if (str_detect(lines_raw$line[1],"\\- [0-9]{1,2} \\-")){
+      de_page <- lines_raw[-1,]
+   } else{
+      de_page <- lines_raw
+   }
+
+   de_page <- de_page %>%
+      rename(German = line) %>%
+      # convert "ſ" to s
+      mutate(German = str_replace_all(German,"ſ","s")) %>%
+      # strip characters that don't ever appear so we know they are false
+      mutate(German = str_remove_all(German,"[\\|\\*<>»;:]")) %>%
+      mutate(German = ifelse(str_detect(German,"[a-zA-Z]3"),str_replace(German,"3","s"),German)) %>%
+      mutate(German = str_squish(German)) %>%
+      # mutate(German = str_replace_all(German,"Ginleitung","Einleitung")) %>%
+      mutate(German = str_replace_all(German,"=","-"))
+
+   words <- tokenizers::tokenize_words(de_page$German,strip_punct = FALSE) %>%
       unlist(recursive = FALSE)
 
 
@@ -99,23 +137,25 @@ process_page <- function(file_name){
    # send to google translate
    # getGoogleLanguages()
    # note we use a character vector not a data frame which is slower and provides less context (less accurate).
+   print("Translating text ")
    translated <- translate(content.vec = cleaned,
                            google.api.key = google.api.key,
                            source.lang = "de",
                            target.lang = "en")
 
    # paginate translation
+   print("Building data frame")
    words_trans <- tokenizers::tokenize_words(translated,strip_punct = FALSE) %>%
       unlist(recursive = FALSE)
 
    words_count <- tokenizers::tokenize_words(translated,strip_punct = FALSE) %>%
       unlist(recursive = FALSE) %>%
       length()
-   words_per_line = round(words_count / nrow(lines))
+   words_per_line = round(words_count / nrow(de_page))
    #build page data frame  do it old school loop since we have to test for punctuation
    word_pos = 1
    cur_page <- list()
-   for (i in 1:nrow(lines)){
+   for (i in 1:nrow(de_page)){
       cur_line = ""
       line_pos = 1
       while(line_pos < words_per_line + 1){
@@ -135,12 +175,12 @@ process_page <- function(file_name){
       mutate(English = str_replace_all(English," \\.",".")) %>%
       mutate(English = str_squish(English))
 
-   content <- bind_cols(lines,en_page)
+   # content <- bind_cols(German,en_page)
 
    processed <- tibble(img_file=file_name,
           ocr_engine = "Tesseract",
           translator = "Google",
-          lines,
+          de_page,
           en_page) %>%
       nest(data = c(line_num,German,English)) %>%
       rename(content = data) %>%
@@ -151,4 +191,12 @@ process_page <- function(file_name){
 }
 
 # main loop -----------------------------------------------------------------------------
-processed <- process_page(file_name)
+NS_Church_History <- tibble()
+NS_Church_History <- dir("img") %>%
+   map(process_page) %>%
+   bind_rows() %>%
+   rownames_to_column(var = "page_num")
+
+save(NS_Church_History,file = "data/ns_church_history.rdata")
+NS_Church_History %>% unnest(content) %>% write_csv("data/ns_church_history.csv")
+
